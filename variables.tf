952246c7b1806c122d23934d45e3b1ba0fc5957e
alinedonatoc <147456852+alinedonatoc@users.noreply.github.com>
2024-05-17 23:54:45 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,18 +6,3 @@ variable "region" {
   description = "AWS region"
 }
 
-variable "table_name" {
-  description = "Dynamodb table name (space is not allowed)"
-  default     = "pagamento-service-table"
-}
-
-variable "table_billing_mode" {
-  description = "Controls how you are charged for read and write throughput and how you manage capacity."
-  default     = "PAY_PER_REQUEST"
-
-}
-
-variable "environment" {
-  description = "Name of environment"
-  default     = "g35"
-}
