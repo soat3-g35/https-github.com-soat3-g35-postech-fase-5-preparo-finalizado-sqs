c8f549f1dc3531b965cd5841c948f3b73ad5ce2d
alinedonatoc <147456852+alinedonatoc@users.noreply.github.com>
2024-05-18 00:01:02 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "aws_sqs_queue" "sh_queue" {
-  name                       = "sh-example-queue"
+  name                       = "update-pagamento-status"
   delay_seconds              = 10
   visibility_timeout_seconds = 30
   max_message_size           = 2048
   message_retention_seconds  = 86400
   receive_wait_time_seconds  = 2
-  sqs_managed_sse_enabled    = true
+  #sqs_managed_sse_enabled    = true
 }
 
 data "aws_iam_policy_document" "sh_sqs_policy" {
