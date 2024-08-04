f8a85481d6c54f0d9723e949acf7b1221e12d539
alinedonatoc <147456852+alinedonatoc@users.noreply.github.com>
2024-08-04 11:55:29 -0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -6,7 +6,7 @@ terraform {
   cloud {
     organization = "DevopsFiapSoat3-G35"
     workspaces {
-      name = "postech-fase-5-pagamento-pendente-sqs"
+      name = "postech-fase-5-pagamento-confirmado-sqs"
     }
   }
 
